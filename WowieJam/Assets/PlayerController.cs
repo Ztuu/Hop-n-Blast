5b02eb07fa4de0004a79368193dd0a06467af8bc
Ztuu <stuartpdev@gmail.com>
2019-02-02 08:12:12 +0000

    Moving player
    Simple player control

---
+++ WowieJam/Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector3 offset;
+    float speed = 5.0f;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        offset = new Vector3(1.0f, 0.0f, 0.0f);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontalInp = Input.GetAxisRaw("Horizontal");
+
+        if(horizontalInp != 0.0f)
+        {
+            transform.position += offset * horizontalInp * Time.deltaTime * speed;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector3(0.0f, 100.0f, 0.0f));
+        }
+    }
+}
