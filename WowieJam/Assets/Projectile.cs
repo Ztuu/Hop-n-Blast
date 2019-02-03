729b6e693c6a6f3d065fb83a8316889e86e75bdd
Ztuu <stuartpdev@gmail.com>
2019-02-03 11:39:20 +0000

    Player development
    Player sprite reflects movement direction and can shoot projectiles based on current direction.

---
+++ WowieJam/Assets/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    float speed = 7.0f;
+    Vector3 offset = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += speed * offset * Time.deltaTime;
+    }
+
+    public void SetDirection(Vector3 newDir)
+    {
+        offset = newDir;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //TODO: What should the projectile do when it hits something
+    }
+}
--- WowieJam/Assets/Scripts/PlayerController.cs
+++ WowieJam/Assets/Scripts/PlayerController.cs
@@ -5,27 +5,70 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Vector3 offset;
-    float speed = 5.0f;
+    float speed = 7.0f;
+    float projDistance = 0.5f; //How far from the player the projectile spawns
     Rigidbody2D rb;
+    SpriteRenderer spriteRend;
 
-    enum PlayerState {IDLE, RUNNING, JUMPING};
+    enum PlayerDirection {LEFT, RIGHT};
+    PlayerDirection playerDirection;
+    enum PlayerState {IDLE, RUNNING, JUMPING, FIRING};
     PlayerState playerState;
+    bool canShoot;
+
+    public GameObject projectile;
+
 
     void Start()
     {
         offset = new Vector3(1.0f, 0.0f, 0.0f);
         rb = GetComponent<Rigidbody2D>();
+        spriteRend = GetComponent<SpriteRenderer>();
+
+        playerState = PlayerState.IDLE;
+        playerDirection = PlayerDirection.RIGHT;
+        canShoot = true;
     }
 
     void Update()
     {
         float horizontalInp = Input.GetAxisRaw("Horizontal");
 
+        if (Input.GetButtonDown("Fire1") && canShoot)
+        {
+            switch (playerState)
+            {
+                case PlayerState.IDLE:
+                    Shoot();
+                    break;
+                case PlayerState.RUNNING:
+                    Shoot();
+                    break;
+                case PlayerState.JUMPING:
+                    ShootDown();
+                    break;
+                case PlayerState.FIRING:
+                    break;
+            }
+        }
+
         if(horizontalInp != 0.0f)
         {
             playerState = PlayerState.RUNNING;
             transform.position += offset * horizontalInp * Time.deltaTime * speed;
-        }else
+
+            if(horizontalInp > 0.0f && playerDirection != PlayerDirection.RIGHT)
+            {
+                playerDirection = PlayerDirection.RIGHT;
+                spriteRend.flipX = false;
+            }
+            else if (horizontalInp < 0.0f && playerDirection != PlayerDirection.LEFT)
+            {
+                playerDirection = PlayerDirection.LEFT;
+                spriteRend.flipX = true;
+            }
+        }
+        else
         {
             playerState = PlayerState.IDLE;
         }
@@ -35,5 +78,45 @@ public class PlayerController : MonoBehaviour
             playerState = PlayerState.JUMPING;
             rb.AddForce(new Vector3(0.0f, 20.0f, 0.0f), ForceMode2D.Impulse);
         }
+    }
+
+    void Shoot()
+    {
+        canShoot = false;
+
+        Vector3 direction = Vector3.zero;
+
+        switch (playerDirection)
+        {
+            case PlayerDirection.RIGHT:
+                direction = new Vector3(1.0f, 0.0f, 0.0f);
+                break;
+            case PlayerDirection.LEFT:
+                direction = new Vector3(-1.0f, 0.0f, 0.0f);
+                break;
+        }
+
+        Vector3 startPos = transform.position + direction * projDistance;
+
+
+        GameObject tempProjRef = Instantiate(projectile, startPos, Quaternion.identity);
+        Projectile tempProjScript = tempProjRef.GetComponent<Projectile>();
+
+        try
+        {
+            tempProjScript.SetDirection(direction);
+        }
+        catch (System.NullReferenceException npe)
+        {
+            Debug.Log(npe.Message.ToString());
+            //TODO: Handle this
+        }
+
+        canShoot = true; 
+    }
+
+    void ShootDown()
+    {
+        canShoot = false;
     }
 }
